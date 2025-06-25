73900fc1f7a8fd8cc98381bfe947120e9f27f575
kislayrajj <wrkislayraj@gmail.com>
2025-06-25 15:55:52 +0530

    unordered list

---
+++ cpp_standard_template_library_(stl)/containers/8_multi_set.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void explain_multi_set();
+
+int main()
+{
+    explain_multi_set();
+    return 0;
+}
+
+void explain_multi_set()
+{
+    // Everything is same as set, only stores duplicate elements too
+
+    multiset<int> ms;
+    ms.insert(1);
+    ms.insert(1);
+    ms.insert(3);
+    ms.insert(2);
+    ms.insert(2);
+    ms.insert(1);
+    // ms.erase(1); // will erase all instance of 1
+
+    // ms.erase(ms.find(1));
+
+    for (auto val : ms)
+    {
+        cout << val << ' ';
+    }
+    cout << endl;
+
+    int cnt = ms.count(1);
+    cout << cnt << endl;
+    // cout<<ms.size()<<endl;
+}
---
+++ cpp_standard_template_library_(stl)/containers/9_unordered_set.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void explain_unordered_set();
+
+int main()
+{
+    explain_unordered_set();
+    return 0;
+}
+
+void explain_unordered_set()
+{
+    // same as set but is not sorted
+    //  lower_bound and upper_bound function does not work, rest all function are same
+    // it does not stores in any particular order, it has a better complexity than set in most cases, except some when collisions happens
+}
