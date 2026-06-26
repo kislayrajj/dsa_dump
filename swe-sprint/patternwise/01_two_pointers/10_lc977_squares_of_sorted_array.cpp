a3ce7666ff4598c67da10479d7394c746e43a1d8
kislayrajj <wrkislayraj@gmail.com>
2026-06-26 22:45:14 +0530

    squares of sorted arrya

---
+++ swe-sprint/patternwise/01_two_pointers/10_lc977_squares_of_sorted_array.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> sortedSquares(vector<int> &arr);
+
+int main()
+{
+    vector<int> arr = {-4, -3, 0, 2, 5, 10};
+    vector<int> result = sortedSquares(arr);
+
+    cout << "{";
+    for (int i = 0; i < result.size(); i++)
+    {
+        cout << result[i];
+
+        if (i != result.size() - 1)
+            cout << ", ";
+    }
+    cout << "}";
+
+    return 0;
+}
+
+// vector<int> sortedSquares(vector<int> &arr)
+// {
+//     int n = arr.size();
+//     vector<int> sortedSquaresArr = {};
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         sortedSquaresArr.push_back(arr[i] * arr[i]);
+//     }
+
+//     sort(sortedSquaresArr.begin(), sortedSquaresArr.end());
+
+//     return sortedSquaresArr;
+// }
+
+// optimal solution with tc O(n)
+
+vector<int> sortedSquares(vector<int> &arr)
+{
+    int n = arr.size();
+    int c = n - 1;
+    int i = 0;
+    int j = n - 1;
+    vector<int> sortedSquaredArr(n);
+
+    while (i <= j)
+    {
+        if (abs(arr[i]) < abs(arr[j]))
+        {
+            sortedSquaredArr[c] = arr[j] * arr[j];
+            c--;
+            j--;
+        }
+        else
+        {
+            sortedSquaredArr[c] = arr[i] * arr[i];
+            c--;
+            i++;
+        }
+    }
+
+    return sortedSquaredArr;
+}
